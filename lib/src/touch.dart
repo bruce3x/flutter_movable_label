09262c3d21262cab67934fc16724ecfe4b2a440c
bruce3x <im.brucezz@gmail.com>
2020-08-10 15:02:13 +0800

    fix: counter must be stateful widget

--- lib/src/touch.dart
+++ lib/src/touch.dart
@@ -2,37 +2,37 @@ import 'package:flutter/material.dart';
 
 typedef TouchPointerCounterCallback = void Function(int count);
 
-class TouchPointerCounter extends StatelessWidget {
+class TouchPointerCounter extends StatefulWidget {
   final Widget child;
   final TouchPointerCounterCallback onChange;
 
-  final pointers = Set<int>();
-
-  TouchPointerCounter({
-    Key key,
-    this.onChange,
-    @required this.child,
-  }) : super(key: key) {
+  TouchPointerCounter({Key key, @required this.child, this.onChange}) : super(key: key) {
     assert(child != null);
   }
 
+  @override
+  _TouchPointerCounterState createState() => _TouchPointerCounterState();
+}
+
+class _TouchPointerCounterState extends State<TouchPointerCounter> {
+  final pointers = Set<int>();
   @override
   Widget build(BuildContext context) {
     return Listener(
       behavior: HitTestBehavior.translucent,
       onPointerDown: (event) {
         pointers.add(event.pointer);
-        onChange?.call(pointers.length);
+        widget.onChange?.call(pointers.length);
       },
       onPointerUp: (event) {
         pointers.remove(event.pointer);
-        onChange?.call(pointers.length);
+        widget.onChange?.call(pointers.length);
       },
       onPointerCancel: (event) {
         pointers.remove(event.pointer);
-        onChange?.call(pointers.length);
+        widget.onChange?.call(pointers.length);
       },
-      child: child,
+      child: widget.child,
     );
   }
 }
